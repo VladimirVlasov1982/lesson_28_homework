bacf8dc3f21e6e8be92e8370e44636f5997a07c8
VladimirVlasov1982 <vladimir.vlasov82@mail.ru>
2022-11-18 09:39:18 +0300

    project ready

--- ads/admin.py
+++ ads/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from ads.models import Categories, Ads
 
-# Register your models here.
+admin.site.register(Categories)
+admin.site.register(Ads)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from users.models import Users, Locations
 
-# Register your models here.
+admin.site.register(Users)
+admin.site.register(Locations)
